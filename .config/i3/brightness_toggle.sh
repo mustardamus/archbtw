e3de5b1d8bd74370bd5469c2d278fcf409bbb7ac
Mustardamus <contact@mustardamus.com>
2025-09-06 12:35:02 +0200

    Increase brightness toggle maximum from 75% to 85%

--- .config/i3/brightness_toggle.sh
+++ .config/i3/brightness_toggle.sh
@@ -5,9 +5,9 @@ current=$(brightnessctl get)
 max=$(brightnessctl max)
 percentage=$((current * 100 / max))
 
-# Toggle between 0% and 75%
+# Toggle between 0% and 85%
 if [ "$percentage" -eq 0 ]; then
-    brightnessctl set 75%
+    brightnessctl set 85%
 else
     brightnessctl set 0%
 fi
