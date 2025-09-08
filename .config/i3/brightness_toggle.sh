6e3b97717e099be69be89316d5cce33ba284e484
Mustardamus <contact@mustardamus.com>
2025-09-08 18:59:56 +0200

    Sync Sway config with i3 settings
    - Match touchpad settings from X11 config
    - Add XF86Display brightness toggle keybind
    - Fix brightness toggle script parsing
    - Add keyboard repeat settings

--- .config/i3/brightness_toggle.sh
+++ .config/i3/brightness_toggle.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Get current brightness percentage
-current=$(brightnessctl get)
-max=$(brightnessctl max)
-percentage=$((current * 100 / max))
+# Get current brightness percentage directly from brightnessctl output
+percentage=$(brightnessctl | grep -oP '\(\K[0-9]+(?=%\))')
 
 # Toggle between 0% and 85%
-if [ "$percentage" -eq 0 ]; then
+if [ "$percentage" -lt 50 ]; then
     brightnessctl set 85%
 else
     brightnessctl set 0%
